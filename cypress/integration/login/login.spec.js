4b708d8f90d542c49ccf2c81354f5491a0cb6eb6
FilipeConde <filipecondep@gmail.com>
2022-08-19 09:46:36 -0300

    Cria classe LoginPage com seus elementos e método principal

--- cypress/integration/login/login.spec.js
+++ cypress/integration/login/login.spec.js
@@ -1,12 +1,10 @@
 /// <reference types="cypress"/>
 
 import { When, Then } from 'cypress-cucumber-preprocessor/steps';
-import { elements } from '../../pages/loginPage';
+import LoginPage from '../../pages/loginPage';
 
 When('logar com usuário {string} e senha {string}', (user, password) => {
-  cy.get(elements.usernameField).type(user);
-  cy.get(elements.passwordField).type(password);
-  cy.get(elements.loginBtn).click();
+  LoginPage.login(user, password);
 });
 Then('deve acessar a página {string}', (urlComplement) => {
   cy.url().should('include', urlComplement);
--- cypress/pages/loginPage.js
+++ cypress/pages/loginPage.js
@@ -1,5 +1,17 @@
+/// <reference types="cypress"/>
+
 export const elements = {
   usernameField: '[data-test="username"]',
   passwordField: '[data-test="password"]',
   loginBtn: '#login-button',
 };
+
+let { usernameField, passwordField, loginBtn } = elements;
+
+export default class LoginPage {
+  static login(username, password) {
+    cy.get(usernameField).type(username);
+    cy.get(passwordField).type(password);
+    cy.get(loginBtn).click();
+  }
+}
